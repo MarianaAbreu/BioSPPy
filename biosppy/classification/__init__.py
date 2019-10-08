0bcbd14e7197de7fb8b0212af859c215364d17e5
MarianaAbreu <33721226+MarianaAbreu@users.noreply.github.com>
2019-10-08 11:22:03 +0100

    Update __init__.py

--- biosppy/classification/__init__.py
+++ biosppy/classification/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import, division, print_function
 
-from . import dissimilarity_based, supervised_learning
+from . import dissimilarity_based, supervised_learning, utils_classification
